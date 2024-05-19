61aec6af66ca7ee14b44c56d4f3e9e890d7fa369
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2024-05-19 23:11:37 +0700

    RoleDTO.java

---
+++ Backend/src/main/java/fpt/edu/vn/Backend/dto/RoleDTO.java
@@ -0,0 +1,14 @@
+package fpt.edu.vn.Backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+public class RoleDTO {
+    private String roleName;
+}
