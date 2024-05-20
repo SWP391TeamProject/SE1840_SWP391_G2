b253fa8fee737e12afd7cbe9cc6701145a396202
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-05-20 15:25:30 +0700

    Revert "fix ConsignmentEntity"

--- Backend/src/main/java/fpt/edu/vn/Backend/pojo/Transaction.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/pojo/Transaction.java
@@ -22,9 +22,6 @@ public class Transaction {
     @Column(length = 30)
     private String type; // DEPOSIT_BALANCE, WITHDRAW_BALANCE, etc.
 
-    enum Type{
-        DEPOSIT_BALANCE, WITHDRAW_BALANCE
-    }
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "account_id")
     private Account account;
@@ -38,18 +35,10 @@ public class Transaction {
     private Integer referenceNum; // Consider changing to Integer for null values
 
     @Column(length = 20, name = "payment_method")
-    private PaymentMethod paymentMethod; // INTERNAL, MOMO, PAYPAL, BANK
-
-    enum PaymentMethod{
-        INTERNAL, MOMO
-    }
+    private String paymentMethod; // INTERNAL, MOMO, PAYPAL, BANK
 
     @Column(length = 30)
     private String status; // SUCCESS, PAYMENT_ERROR, INSUFFICIENT_BALANCE
-
-    enum Status{
-        SUCCESS, PAYMENT_ERROR, INSUFFICIENT_BALANCE
-    }
 
     @CreationTimestamp
     @Column(name = "create_date")
