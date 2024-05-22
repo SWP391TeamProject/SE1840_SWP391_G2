e7b888ed97ed3c4a257290d3afd9377a7df81294
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-05-22 18:31:20 +0700

    Update Attachment service

---
+++ Backend/src/main/java/fpt/edu/vn/Backend/service/AttachmentServiceImpl.java
@@ -0,0 +1,83 @@
+package fpt.edu.vn.Backend.service;
+
+import fpt.edu.vn.Backend.pojo.Attachment;
+import fpt.edu.vn.Backend.repository.AttachmentRepos;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.WritableResource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class AttachmentServiceImpl implements AttachmentService {
+    private final AttachmentRepos attachmentRepository;
+    private final ResourceLoader resourceLoader;
+
+    @Autowired
+    public AttachmentServiceImpl(AttachmentRepos attachmentRepository, @Qualifier("azureStorageFileProtocolResolver") ResourceLoader resourceLoader) {
+        this.attachmentRepository = attachmentRepository;
+        this.resourceLoader = resourceLoader;
+    }
+
+    public String getResourcePath(String containerName, String blobId) {
+        return String.format("azure-blob://%s/%s", containerName, blobId);
+    }
+
+    @Override
+    public Attachment uploadAttachment(MultipartFile file, int entityId, Attachment.EntityType entityType) throws IOException {
+        String blobId = UUID.randomUUID().toString();
+        String resourcePath = getResourcePath(entityType.toString(), blobId);
+        Resource blobResource = resourceLoader.getResource(resourcePath);
+        try (OutputStream os = ((WritableResource) blobResource).getOutputStream()) {
+            os.write(file.getBytes());
+        }
+        String directLink = blobResource.getURI().toString();
+        Attachment.FileType fileType = Attachment.FileType.UNKNOWN;
+        if (file.getContentType() != null) {
+            if (file.getContentType().startsWith("image")) {
+                fileType = Attachment.FileType.IMAGE;
+            } else if (file.getContentType().startsWith("video")) {
+                fileType = Attachment.FileType.VIDEO;
+            }
+        }
+        Attachment attachment = new Attachment(
+                null,
+                directLink, fileType,
+                entityId, entityType,
+                null, null);
+        attachmentRepository.save(attachment);
+        return attachment;
+    }
+
+    @Override
+    public Attachment getAttachmentById(int id) {
+        return attachmentRepository.findByAttachmentId(id);
+    }
+
+    @Override
+    public List<Attachment> getAllAttachments() {
+        return attachmentRepository.findAll();
+    }
+
+    @Override
+    public List<Attachment> getAllAttachmentsByEntity(int entityId, Attachment.EntityType entityType) {
+        return attachmentRepository.findByEntityIdAndEntityType(entityId, entityType);
+    }
+
+    @Override
+    public void deleteAttachment(int id) {
+        attachmentRepository.deleteByAttachmentId(id);
+    }
+
+    @Override
+    public void deleteAttachmentsByEntity(int entityId, Attachment.EntityType entityType) {
+        attachmentRepository.deleteByEntityIdAndEntityType(entityId, entityType);
+    }
+}
