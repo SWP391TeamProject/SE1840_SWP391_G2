89f32985c234f44a0b25f267f3902aadcb624545
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-05-27 16:52:03 +0700

    fix applicaiton.properties to use

--- frontend/src/pages/Consignment/DropZoneComponent.tsx
+++ frontend/src/pages/Consignment/DropZoneComponent.tsx
@@ -1,35 +1,43 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { useDropzone } from "react-dropzone";
+interface AcceptedFile extends File {
+  preview: string;
+}
+export default function DropzoneComponent(props) {
+  const [acceptedFiles, setAcceptedFiles] = useState<AcceptedFile[]>([]); // Explicit typing
+  const { getRootProps, getInputProps, fileRejections } = useDropzone({
+    accept: { "image/*": [] },
+    onDrop: (newAcceptedFiles) => {
+      // Rename to clarify the new files
+      setAcceptedFiles((prevAcceptedFiles) => [
+        ...prevAcceptedFiles, // Keep the existing files
+        ...newAcceptedFiles.map((file) =>
+          Object.assign(file, { preview: URL.createObjectURL(file) })
+        ),
+      ]);
+    },
+  });
 
-const DropzoneComponent = () => {
-  const [files, setFiles] = useState([]);
-  const onDrop = useCallback((acceptedFiles) => {
-    setFiles(
-      acceptedFiles.map((file) =>
-        Object.assign(file, {
-          preview: URL.createObjectURL(file),
-        })
-      )
-    );
-    console.log(acceptedFiles);
-  }, []);
-
-  const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
-  const fileList = files.map((file) => (
-    <li key={file.name}>
-      <img src={file.preview} alt={file.name} />
-      <span>{file.name}</span>
+  const acceptedFileItems = acceptedFiles.map((file) => (
+    <li key={file.path}>
+      {/* Display image preview */}
+      <img src={file.preview} alt={file.name} style={{ maxWidth: "100px" }} />
+      {file.path} - {file.size} bytes
     </li>
   ));
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      <p>Drag and drop your files here, or click to select files</p>
-      <ul>{fileList}</ul>
-    </div>
+    <section className="container">
+      <div {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        <p>Drag 'n' drop some images here, or click to select images</p>
+      </div>
+      <aside>
+        <h4>Accepted files</h4>
+        <ul>{acceptedFileItems}</ul>
+        <h4>Rejected files</h4>
+        {/* <ul>{fileRejectionItems}</ul> */}
+      </aside>
+    </section>
   );
-};
-
-export default DropzoneComponent;
+}
