ae002fc7291071d4e4c1fd9060a166cb9ba44438
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-05-30 10:25:56 +0700

    dai hoc fpt hang dau vn

--- Backend/src/main/java/fpt/edu/vn/Backend/DTO/AccountDTO.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/DTO/AccountDTO.java
@@ -1,14 +1,13 @@
 package fpt.edu.vn.Backend.DTO;
 
 import fpt.edu.vn.Backend.pojo.Account;
-import fpt.edu.vn.Backend.pojo.Role;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.List;
+import java.util.Set;
 
 @Data
 @NoArgsConstructor
@@ -16,28 +15,12 @@ import java.util.List;
 public class AccountDTO {
     private Integer accountId;
     private String nickname;
-    private List<RoleDTO> role;
+    private Set<Account.Role> roles;
     private AttachmentDTO avatar;
     private String email;
     private String phone;
-    private Byte status;
+    private Account.Status status;
     private BigDecimal balance;
     private LocalDateTime createDate;
     private LocalDateTime updateDate;
-
-    // getters and setters
-
-    public AccountDTO(Account account) {
-        this.accountId = account.getAccountId();
-        this.nickname = account.getNickname();
-        this.email = account.getEmail();
-        this.phone = account.getPhone();
-        this.status = account.getStatus();
-        this.balance = account.getBalance();
-        this.createDate = account.getCreateDate();
-        this.updateDate = account.getUpdateDate();
-        this.role = account.getAuthorities().stream().map(RoleDTO::new).toList();
-    }
-
-
 }
--- Backend/src/main/java/fpt/edu/vn/Backend/security/CustomUserDetailsService.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/security/CustomUserDetailsService.java
@@ -1,7 +1,6 @@
 package fpt.edu.vn.Backend.security;
 
 import fpt.edu.vn.Backend.pojo.Account;
-import fpt.edu.vn.Backend.pojo.Role;
 import fpt.edu.vn.Backend.repository.AccountRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
@@ -31,7 +30,7 @@ public class CustomUserDetailsService  implements UserDetailsService {
         return new User(user.getEmail(), user.getPassword(), mapRolesToAuthorities(user.getAuthorities()));
     }
 
-    private Collection<GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
-        return roles.stream().map(role -> new SimpleGrantedAuthority(String.valueOf(role.getRoleName()))).collect(Collectors.toList());
+    private Collection<GrantedAuthority> mapRolesToAuthorities(Collection<Account.Role> roles) {
+        return roles.stream().map(role -> new SimpleGrantedAuthority(role.toString())).collect(Collectors.toSet());
     }
 }
