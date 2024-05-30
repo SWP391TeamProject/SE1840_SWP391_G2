8d69aa1ac01440b14751707e4431e6dba19653ef
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-05-30 12:50:40 +0700

    Reapply "Update account & roles"
    This reverts commit d8e2ccb52c963f9b2d0e6f7a366c99e103e29611.

--- Backend/src/main/java/fpt/edu/vn/Backend/service/AccountService.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/service/AccountService.java
@@ -4,22 +4,29 @@ import fpt.edu.vn.Backend.DTO.AccountDTO;
 import fpt.edu.vn.Backend.DTO.AttachmentDTO;
 import fpt.edu.vn.Backend.pojo.Account;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Set;
+
 public interface AccountService {
-    Page<AccountDTO> getAllAccounts(@NotNull Pageable pageable);
-    AccountDTO createAccount( AccountDTO account);
-    AccountDTO getAccountById(int id);
-    AccountDTO updateAccount(AccountDTO account);
-    AccountDTO getAccountByEmail(String username);
-    AccountDTO getAccountByEmailAndPassword(String email, String password);
-    Account parseAccountDTOToEntity(AccountDTO accountDTO) ;
+    @NotNull AccountDTO mapEntityToDTO(@NotNull Account account, @NotNull AccountDTO accountDTO);
+    @NotNull default AccountDTO mapEntityToDTO(@NotNull Account account) {
+        return mapEntityToDTO(account, new AccountDTO());
+    }
+    @NotNull Account mapDTOToEntity(@NotNull AccountDTO accountDTO, @NotNull Account account);
 
-    AttachmentDTO setAvatar(int id, MultipartFile file);
+    @NotNull Page<AccountDTO> getAccounts(@NotNull Pageable pageable);
+    @NotNull Page<AccountDTO> getAccountsByRoles(@NotNull Pageable pageable, Set<Account.Role> roles);
 
-    void activateAccount(int id);
+    @Nullable AccountDTO getAccountById(int accountId);
+    @Nullable AccountDTO getAccountByEmail(@NotNull String email);
+    @Nullable AccountDTO getAccountByEmailAndPassword(@NotNull String email, @NotNull String password);
 
-    void deactivateAccount(int id);
+    @NotNull AccountDTO createAccount(@NotNull AccountDTO account);
+    @NotNull AccountDTO updateAccount(@NotNull AccountDTO account);
+
+    @NotNull AttachmentDTO setAvatar(int accountId, @NotNull MultipartFile file);
 }
