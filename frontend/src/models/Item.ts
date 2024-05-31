3501c699112cf0dae6436040882903ec31bb5f4f
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-05-31 15:23:35 +0700

    Update item service

---
+++ frontend/src/models/Item.ts
@@ -0,0 +1,21 @@
+export interface Item {
+  itemId?: number;
+  categoryId?: number;
+  name: string;
+  description: string;
+  reservePrice: number;
+  buyInPrice: number;
+  status: ItemStatus;
+  createDate?: string;
+  updateDate?: string;
+  ownerId?: number;
+  orderId?: number;
+}
+
+export enum ItemStatus {
+  VALUATING = "VALUATING",
+  QUEUE = "QUEUE",
+  IN_AUCTION = "IN_AUCTION",
+  SOLD = "SOLD",
+  UNSOLD = "UNSOLD"
+}
