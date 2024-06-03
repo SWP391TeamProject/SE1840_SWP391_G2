463d3aa73c1795735aafdf3205a50fe1148f64c3
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-06-03 09:51:36 +0700

    Fix API endpoint

--- frontend/src/constants/domain.ts
+++ frontend/src/constants/domain.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV === 'development';
-export const API_SERVER: string = dev ? "https://localhost:8080/api/" : "/api/";
-export const AUTH_SERVER: string = dev ? "https://localhost:8080/auth/" : "/auth/";
+export const API_SERVER: string = dev ? "http://localhost:8080/api" : "/api/";
+export const AUTH_SERVER: string = dev ? "http://localhost:8080/auth" : "/auth/";
