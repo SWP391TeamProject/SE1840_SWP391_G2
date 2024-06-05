08fa2b8b64c116bbfae02100b6b73dd603931e7f
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-06-05 13:39:27 +0700

    Update

--- Backend/src/main/java/fpt/edu/vn/Backend/repository/AuctionItemRepos.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/repository/AuctionItemRepos.java
@@ -4,7 +4,5 @@ import fpt.edu.vn.Backend.pojo.AuctionItem;
 import fpt.edu.vn.Backend.pojo.AuctionItemId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface AuctionItemRepos extends JpaRepository<AuctionItem, AuctionItemId> {
 }
