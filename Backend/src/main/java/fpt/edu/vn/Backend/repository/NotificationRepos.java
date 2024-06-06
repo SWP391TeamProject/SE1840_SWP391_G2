effe5b5f5a3e0988d8c52d443ced0fa6cfebc650
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-06-06 16:35:37 +0700

    Add notification service

--- Backend/src/main/java/fpt/edu/vn/Backend/repository/NotificationRepos.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/repository/NotificationRepos.java
@@ -1,16 +1,12 @@
 package fpt.edu.vn.Backend.repository;
 
 import fpt.edu.vn.Backend.pojo.Notification;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-@RepositoryRestResource(collectionResourceRel = "notifications", path = "notifications")
-public interface NotificationRepos extends PagingAndSortingRepository<Notification, Integer> {
-
+public interface NotificationRepos extends JpaRepository<Notification, Integer> {
+    Page<Notification> findNotificationByAccount_EmailOrderByCreateDateDesc(String userEmail, Pageable pageable);
 }
