aabf93daf5d9e94c0bd20b9ca5a36627f768cddf
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2024-06-06 14:42:45 +0700

    Implement Auto Delete ExpiryTime Token

--- Backend/src/main/java/fpt/edu/vn/Backend/oauth2/security/TokenProvider.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/oauth2/security/TokenProvider.java
@@ -1,17 +1,12 @@
 package fpt.edu.vn.Backend.oauth2.security;
 
 import com.nimbusds.jose.*;
-import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.MACVerifier;
-import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import fpt.edu.vn.Backend.DTO.request.IntrospectRequest;
-import fpt.edu.vn.Backend.DTO.request.LogOutRequest;
 import fpt.edu.vn.Backend.DTO.response.IntrospectResponse;
 import fpt.edu.vn.Backend.oauth2.exception.AppException;
 import fpt.edu.vn.Backend.oauth2.exception.ErrorCode;
-import fpt.edu.vn.Backend.pojo.Account;
-import fpt.edu.vn.Backend.pojo.InvalidatedToken;
 import fpt.edu.vn.Backend.repository.InvalidatedTokenRepos;
 import fpt.edu.vn.Backend.security.SecurityConstants;
 import io.jsonwebtoken.*;
@@ -20,17 +15,20 @@ import lombok.experimental.NonFinal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
 import java.text.ParseException;
-import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.temporal.ChronoUnit;
 import java.util.Date;
 
-import java.util.UUID;
 
 @Service
 public class TokenProvider {
@@ -123,4 +121,12 @@ public class TokenProvider {
 
         return IntrospectResponse.builder().valid(isValid).build();
     }
+
+    @Scheduled(fixedRate = 5000000) // Run every 50 minute
+    @Transactional
+    public void cleanupExpiredTokens() {
+        Date now = Date.from(LocalDateTime.now().atZone(ZoneId.systemDefault()).toInstant());
+        invalidatedTokenRepos.deleteByExpiryTimeBefore(now);
+    }
+
 }
--- Backend/src/main/java/fpt/edu/vn/Backend/repository/InvalidatedTokenRepos.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/repository/InvalidatedTokenRepos.java
@@ -3,7 +3,15 @@ package fpt.edu.vn.Backend.repository;
 import fpt.edu.vn.Backend.pojo.InvalidatedToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.Optional;
 
 @Repository
 public interface InvalidatedTokenRepos extends JpaRepository<InvalidatedToken, String> {
+    @Transactional
+    void deleteByExpiryTimeBefore(Date now);
+
 }
