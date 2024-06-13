a2fb78cf4c531afd8cb457c6c6dc4fde37b9bba5
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-06-14 00:35:16 +0700

    fix deployment config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'development' ? '/' : 'http://hello.world/',
+  base: '/' ,
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
