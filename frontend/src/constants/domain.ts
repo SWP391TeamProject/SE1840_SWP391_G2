cb9f0af56bf9140c68d1af15f2a89863a1911953
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-06-14 21:02:43 +0700

    adjust it to work  with the new env file

--- frontend/src/constants/domain.ts
+++ frontend/src/constants/domain.ts
@@ -1,9 +1,9 @@
-export const SERVER_DOMAIN_URL : string = "http://localhost:8080";
+export const SERVER_DOMAIN_URL: string = process.env.SERVER_DOMAIN_URL || "http://localhost:8080";
 
+export const API_SERVER: string = process.env.API_SERVER || "http://localhost:8080/api";
 
-export const API_SERVER : string = "http://localhost:8080/api";
+export const AUTH_SERVER: string = process.env.AUTH_SERVER || "http://localhost:8080/auth";
 
-export const AUTH_SERVER : string = "http://localhost:8080/auth";
 
 
 
