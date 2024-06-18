7466545eab29f4e2cf62ed2d226b47d0e0d14d4d
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-06-18 17:36:36 +0700

    add web config

---
+++ Backend/src/main/java/fpt/edu/vn/Backend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package fpt.edu.vn.Backend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Value("${FRONTEND_CORS_SERVER}")
+    private  String FRONTEND_SERVER_URL;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(FRONTEND_SERVER_URL)
+                .allowedMethods("GET","POST","PUT","DELETE","HEAD","PATCH")
+                .allowedHeaders("Authorization", "Content-Type", "X-Auth-Token", "Access-Control-Allow-Origin")
+                .allowCredentials(true)
+        ;
+    }
+}
