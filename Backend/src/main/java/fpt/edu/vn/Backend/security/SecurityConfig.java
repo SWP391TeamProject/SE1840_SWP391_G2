20648d4bd7c6e80fcf2787812117c3bce2da7861
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-06-19 13:56:50 +0700

    fix #251

--- Backend/src/main/java/fpt/edu/vn/Backend/security/SecurityConfig.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/security/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
                         AbstractHttpConfigurer::disable
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**","api/auction-sessions/**","/auction-join/**","/api/item-categories/**","/test","/.well-known/**","/api/blogs/**").permitAll()
+                        .requestMatchers("/auth/**","api/auction-sessions/**","/auction-join/**","/api/item-categories/**","/test","/.well-known/**","/api/blogs/**","/api/payments/vnpay_ipn").permitAll()
                         .anyRequest().authenticated()
 
                 )
