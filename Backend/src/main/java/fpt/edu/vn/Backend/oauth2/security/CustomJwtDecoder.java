201a0e2c2ab0d43ec76b0407d4bf77b042c39d50
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-06-19 11:53:44 +0700

    attemp fix jwt#4

--- Backend/src/main/java/fpt/edu/vn/Backend/oauth2/security/CustomJwtDecoder.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/oauth2/security/CustomJwtDecoder.java
@@ -1,31 +1,30 @@
-package fpt.edu.vn.Backend.oauth2.security;
-
-import java.text.ParseException;
-import java.util.Objects;
-import javax.crypto.spec.SecretKeySpec;
-
-import com.nimbusds.jose.JOSEException;
-import fpt.edu.vn.Backend.DTO.request.IntrospectRequest;
-import fpt.edu.vn.Backend.security.SecurityConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.JwtException;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.stereotype.Component;
-
-@Component
-public class CustomJwtDecoder  {
+//package fpt.edu.vn.Backend.oauth2.security;
+//
+//import java.text.ParseException;
+//import java.util.Objects;
+//import javax.crypto.spec.SecretKeySpec;
+//
+//import com.nimbusds.jose.JOSEException;
+//import fpt.edu.vn.Backend.DTO.request.IntrospectRequest;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+//import org.springframework.security.oauth2.jwt.Jwt;
+//import org.springframework.security.oauth2.jwt.JwtDecoder;
+//import org.springframework.security.oauth2.jwt.JwtException;
+//import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//public class CustomJwtDecoder implements JwtDecoder {
 //    @Value("${jwt.signerKey}")//dang loi cho nay
-    private String signerKey = SecurityConstants.JWT_SECRET;
-
-    @Autowired
-    private TokenProvider tokenProvider;
-
-    private NimbusJwtDecoder nimbusJwtDecoder = null;
-
+//    private String signerKey;
+//
+//    @Autowired
+//    private TokenProvider tokenProvider;
+//
+//    private NimbusJwtDecoder nimbusJwtDecoder = null;
+//
 //    @Override
 //    public Jwt decode(String token) throws JwtException {
 //
@@ -47,4 +46,4 @@ public class CustomJwtDecoder  {
 //
 //        return nimbusJwtDecoder.decode(token);
 //    }
-}
+//}
