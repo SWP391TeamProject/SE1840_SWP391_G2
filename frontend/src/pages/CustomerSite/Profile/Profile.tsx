dd998d8334a662b2022c5f23e06827081bad6d5f
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-06-24 14:15:50 +0700

    Fix profile responsive

--- frontend/src/pages/CustomerSite/Profile/Profile.tsx
+++ frontend/src/pages/CustomerSite/Profile/Profile.tsx
@@ -18,53 +18,30 @@ const profileNavItems = [
 ];
 
 export default function Profile() {
-  const location = useLocation();
-
   return (
     <>
-      <div className="container min-h-screen m-3 flex flex-row bg-background text-foreground">
+      <div className="container min-h-screen flex flex-col md:flex-row gap-8 md:gap-16 bg-background text-foreground py-8">
         {/* Vertical Sidebar */}
-        <div className="w-64 lg:w-80 min-h-screen border-gray-200 bg-background text-foreground hidden md:flex flex-col">
-          <aside className="w-64 lg:w-80   border-gray-200 p-4 hidden lg:block fixed bg-background text-foreground">
-            <h2 className="text-lg font-semibold mb-4">Profile</h2>
-            <Separator />
-            <nav className="mt-4 bg-inherit text-inherit">
-              {profileNavItems.map((item) => (
-                <NavLink
-                  key={item.href}
-                  to={item.href}
-                  className={({ isActive }) =>
-                    cn(
-                      "block px-4 py-2 rounded-md text-sm font-medium hover:bg-accent hover:text-accent-foreground transition-colors",
-                      isActive ? "bg-accent text-accent-foreground " : ""
-                    )
-                  }
-                >
-                  {item.title}
-                </NavLink>
-              ))}
-            </nav>
-          </aside>
-        </div>
+        <nav className="w-64 md:min-h-screen border-gray-200 flex flex-row md:flex-col">
+          {profileNavItems.map((item) => (
+            <NavLink
+              key={item.href}
+              to={item.href}
+              className={({ isActive }) =>
+                cn(
+                  "block px-4 py-2 rounded-md text-sm font-medium hover:bg-accent hover:text-accent-foreground transition-colors",
+                  isActive ? "bg-accent text-accent-foreground " : ""
+                )
+              }
+            >
+              {item.title}
+            </NavLink>
+          ))}
+        </nav>
 
         {/* Main Content Area */}
-        <div className="flex-1 lg:pl-8 xl:pl-16 bg-background text-foreground p-3">
-          <Breadcrumb
-            className="py-4"
-            links={[
-              {
-                href: "/profile",
-                label: "Profile",
-              },
-              {
-                href: location.pathname,
-                label: profileNavItems.find((item) => item.href === location.pathname)
-                  ?.title || "",
-              },
-            ] as LinkProps[]}
-          />
-
-          <div className="h-full">
+        <div className="grow">
+          <div className="max-w-2xl m-auto">
             <Outlet />
           </div>
         </div>
