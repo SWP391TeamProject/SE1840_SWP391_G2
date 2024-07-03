be1e6d932d21546811f5c37e274e1764ac5e7f55
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-07-03 20:34:33 +0700

    Fix cache kv issue

--- Backend/src/main/java/fpt/edu/vn/Backend/config/RedisConfig.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/config/RedisConfig.java
@@ -13,8 +13,9 @@ public class RedisConfig {
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
         return template;
     }
 }
