7302b624a741b6ca8f379e9e36be95207a62b6a3
phucvxse180225 <169065491+phucvjp@users.noreply.github.com>
2024-07-03 13:11:58 +0700

    impl orders api

--- Backend/src/main/java/fpt/edu/vn/Backend/DTO/OrderDTO.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/DTO/OrderDTO.java
@@ -4,6 +4,8 @@ import fpt.edu.vn.Backend.pojo.Order;
 import lombok.*;
 
 import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -12,11 +14,15 @@ import java.util.stream.Collectors;
 public class OrderDTO implements Serializable {
     private int orderId;
     private Set<ItemDTO> item;
+    private String shippingAddress;
     private PaymentDTO payment;
+    private LocalDateTime createDate;
 
     public OrderDTO(Order order){
         this.orderId = order.getOrderId();
         this.item = order.getItems().stream().map(ItemDTO::new).collect(Collectors.toSet());
         this.payment = new PaymentDTO(order.getPayment());
+        this.shippingAddress = order.getShippingAddress();
+        this.createDate = order.getCreateDate();
     }
 }
