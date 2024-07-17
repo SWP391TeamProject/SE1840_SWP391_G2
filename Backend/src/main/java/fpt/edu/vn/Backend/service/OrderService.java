4c179e89fa522589592ac3b209d1dbef7fb90479
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-07-17 23:57:39 +0700

    fix orderservice missing code

--- Backend/src/main/java/fpt/edu/vn/Backend/service/OrderService.java
+++ Backend/src/main/java/fpt/edu/vn/Backend/service/OrderService.java
@@ -28,7 +28,6 @@ public interface OrderService {
 
     Page<OrderDTO> getAllOrdersByUserIdAndStatus(int userId, Payment.Status status, Pageable pageable);
 
-    OrderDTO updateOrderShippingStatus(Order.ShippingStatus shippingStatus, int orderId);
 
     OrderDTO updateOrder(int id, OrderUpdateDTO dto);
 }
