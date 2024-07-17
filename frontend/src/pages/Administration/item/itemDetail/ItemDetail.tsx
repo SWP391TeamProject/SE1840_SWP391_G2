7f2a8d50e907fa831d2280a8ddd2f871a44ee7fe
cuong1903 <cuong.tran.vt19@gmail.com>
2024-07-17 20:00:20 +0700

    update

--- frontend/src/pages/Administration/item/itemDetail/ItemDetail.tsx
+++ frontend/src/pages/Administration/item/itemDetail/ItemDetail.tsx
@@ -185,6 +185,7 @@ export default function ItemDetail() {
           message={"Item " + currentItem?.name}
           label="Ok"
           onSuccess={confirm}
+          description=""
         />
     </>
   );
