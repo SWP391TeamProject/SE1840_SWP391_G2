56232a320f329427371a364e967e3cf55baca811
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-07-22 23:52:57 +0700

    change method post to get for login with google

--- frontend/src/pages/authentication/LoginForm.tsx
+++ frontend/src/pages/authentication/LoginForm.tsx
@@ -91,7 +91,7 @@ function LoginForm() {
     console.log(token);
     if (token !== null) {
       axios
-        .post(AUTH_SERVER + '/login-with-google?token=' + token)
+        .get(AUTH_SERVER + '/login-with-google?token=' + token)
         .then((res) => {
           setIsLogin(false);
           console.log(res.data);
