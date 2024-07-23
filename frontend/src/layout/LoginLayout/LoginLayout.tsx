9134950288d982d3547b7ea52447a3a22e3a4174
Vi Le <115398831+vidz231@users.noreply.github.com>
2024-07-24 01:01:09 +0700

    Update LoginLayout.tsx

--- frontend/src/layout/LoginLayout/LoginLayout.tsx
+++ frontend/src/layout/LoginLayout/LoginLayout.tsx
@@ -1,12 +1,12 @@
 import RegisterForm from '@/pages/authentication/RegisterForm';
 import { Suspense, lazy } from 'react';
-const LoginForm = lazy(() => import('@/pages/authentication/LoginForm'));
+// const LoginForm = lazy(() => import('@/pages/authentication/LoginForm'));
 export default function LoginLayout() {
   return (
     <>
       <div className="flex h-screen justify-center align-middle items-center ">
         <Suspense fallback={<div>Loading...</div>}>
-          <LoginForm />
+          {/* <LoginForm /> */}
           <RegisterForm />
         </Suspense>
       </div>
