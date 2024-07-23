ba05dae0a61e18f39dfb24c04f1a2102433764d5
cuong1903 <cuong.tran.vt19@gmail.com>
2024-07-23 23:58:09 +0700

    Add Schedule when create Auction Session

--- frontend/src/pages/Administration/Auction-session/AuctionSessionCreate.tsx
+++ frontend/src/pages/Administration/Auction-session/AuctionSessionCreate.tsx
@@ -1,13 +1,13 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import { z } from 'zod';
+import { custom, z } from 'zod';
 
 import DropzoneComponent from '@/components/drop-zone/DropZoneComponent';
 import { Button } from '@/components/ui/button';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { ScrollArea } from '@/components/ui/scroll-area';
-import { createAuctionSession } from '@/services/AuctionSessionService';
+import { createAuctionSession, getAuctions } from '@/services/AuctionSessionService';
 import { Loader2 } from 'lucide-react';
 import { useEffect, useState } from 'react';
 import { toast } from 'sonner';
@@ -18,6 +18,8 @@ import { useNavigate } from 'react-router-dom';
 import { setCurrentAuctionSession } from '@/redux/reducers/AuctionSession';
 import { useAppDispatch } from '@/redux/hooks';
 import { Textarea } from '@/components/ui/textarea';
+import { AuctionSessionStatus } from '@/constants/enums';
+import { Scheduler } from "@aldabil/react-scheduler";
 
 const FormSchema = z.object({
   title: z.string().min(2, {
@@ -38,6 +40,10 @@ const FormSchema = z.object({
 
 export default function AuctionSessionCreate() {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [scheduledSessions, setScheduledSessions] = useState([]);
+  const [events, setEvents] = useState([]);
+  const [step, setStep] = useState(1);
+  const [scheduleTrigger, setScheduleTrigger] = useState(false);
   const nav = useNavigate();
   const dispatch = useAppDispatch();
 
@@ -48,6 +54,7 @@ export default function AuctionSessionCreate() {
       files: [],
     },
   });
+
   const [isConfirmed, setIsConfirmed] = useState(false);
   const [showTrigger, setShowTrigger] = useState(false);
   const handleConfirmed = (values: z.infer<typeof FormSchema>) => {
@@ -84,109 +91,257 @@ export default function AuctionSessionCreate() {
   }, [isConfirmed, form.getValues]);
 
   function onSubmit(data: z.infer<typeof FormSchema>) {
-    console.log(data);
-    console.log('validated');
-    setShowTrigger(true);
+    if (multipleDateRangeOverlaps(events)) {
+      form.setError('startDate', { type: "custom", message: "New Session time range is overlapping another Scheduled Session." });
+      form.setError('endDate', { type: "custom", message: "" })
+    } else {
+      form.clearErrors('startDate');
+      form.clearErrors('endDate');
+      console.log(data);
+      console.log('validated');
+      setScheduleTrigger(false);
+      setShowTrigger(true);
+    }
   }
 
-  return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6 p-3">
-        {/* this is the title of the auction session. */}
-        <FormField
-          control={form.control}
-          name="title"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Title</FormLabel>
-              <FormControl>
-                <Input placeholder="Auction Session" {...field} />
-              </FormControl>
-              <FormDescription>This is the title of the auction session.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <div className="flex items-center justify-start gap-4">
-          {/* This is the start date of the auction session. */}
-          <FormField
-            control={form.control}
-            name="startDate"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>Start Date</FormLabel>
-                <FormControl>
-                  <input type="datetime-local" className="cursor-pointer bg-background text-foreground" {...field} />
-                </FormControl>
-                <FormDescription>this is the start date of the auction session</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
+  const goNextStep = () => {
+    setStep(step + 1);
+  }
 
-          {/* This is the end date of the auction session. */}
-          <FormField
-            control={form.control}
-            name="endDate"
-            render={({ field }) => (
-              <FormItem className="flex flex-col">
-                <FormLabel>End Date</FormLabel>
-                <FormControl>
-                  <input type="datetime-local" className="cursor-pointer bg-background text-foreground" {...field} />
-                </FormControl>
-                <FormDescription>this is the end date of the auction session</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        </div>
-        <FormField
-          control={form.control}
-          name="description"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Description</FormLabel>
-              <FormControl>
-                <Textarea placeholder="description of the session" {...field} />
-              </FormControl>
-              <FormDescription>This is the description of the auction session.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+  const goPreviousStep = () => {
+    setStep(step - 1);
+  }
 
-        {/* This is the attachments for the auction session. */}
-        <ScrollArea className="w-full h-64 overflow-hidden">
-          <FormField
-            control={form.control}
-            name="files"
-            render={({ field }) => <DropzoneComponent {...field} control={form.control} />}
-          />
-        </ScrollArea>
-        {isSubmitting ? (
-          <Button variant="default" disabled>
-            <Loader2 className="animate-spin" size={24} />
-            Submitting...
-          </Button>
-        ) : (
-          <Button type="submit" variant="default">
-            Submit
-          </Button>
-        )}
-      </form>
-      <ConfirmationDialog
-        open={showTrigger}
-        description="Are you sure to create this auction session?"
-        onOpenChange={setShowTrigger}
-        title="Are you sure to create this auction session?"
-        message={
-          form.formState.isDirty
-            ? `Auction Session ${form.getValues()?.title || ''} will be created.\n Start Date: ${form.getValues()?.startDate ? formatDateTime(form.getValues()?.startDate) : 'N/A'} \n End Date: ${form.getValues()?.endDate ? formatDateTime(form.getValues().endDate) : 'N/A'}`
-            : ''
+  const handleViewSchedule = () => {
+    setScheduleTrigger(!scheduleTrigger);
+  }
+
+  const handleDateChange = () => {
+    let start = form.control._formValues['startDate'];
+    let end = form.control._formValues['endDate'];
+    console.log(start);
+    console.log(end);
+    if (start && end) {
+      let newSession = {
+        event_id: null,
+        title: form.control._formValues['title'] || "New Auction Session",
+        start: new Date(start),
+        end: new Date(end),
+        color: "orange",
+      }
+      console.log(scheduledSessions);
+      let tempList = [...scheduledSessions];
+      tempList.push(newSession);
+      console.log(tempList);
+      setEvents(tempList);
+      if (multipleDateRangeOverlaps(tempList)) {
+        form.setError('startDate', {type: "custom", message:"New Session time range is overlapping another Scheduled Session."});
+        form.setError('endDate', {type: "custom", message:""})
+      } else {
+        form.clearErrors('startDate');
+        form.clearErrors('endDate');
+      }      
+    }
+  }
+
+  function dateRangeOverlaps(a_start, a_end, b_start, b_end) {
+    if (a_start < b_start && b_start < a_end) return true; // b starts in a
+    if (a_start < b_end && b_end < a_end) return true; // b ends in a
+    if (b_start < a_start && a_end < b_end) return true; // a in b
+    return false;
+  }
+
+  function multipleDateRangeOverlaps(timeEntries) {
+    let i = 0, j = 0;
+    let timeIntervals = timeEntries.filter(entry => entry.start != null && entry.end != null);
+
+    if (timeIntervals != null && timeIntervals.length > 1)
+      for (i = 0; i < timeIntervals.length - 1; i += 1) {
+        for (j = i + 1; j < timeIntervals.length; j += 1) {
+          if (
+            dateRangeOverlaps(
+              timeIntervals[i].start, timeIntervals[i].end,
+              timeIntervals[j].start, timeIntervals[j].end
+            )
+          ) return true;
         }
-        label="Confirm"
-        onSuccess={confirm}
-      />
-    </Form>
+      }
+    return false;
+  }
+
+  useEffect(() => {
+    getAuctions({ page: 1, size: 100, status: AuctionSessionStatus.SCHEDULED }).then((res) => {
+      console.log(res.data);
+      let sessions = res.data.content.map((session) => (
+        {
+          event_id: session.auctionSessionId,
+          title: session.title,
+          start: new Date(session.startDate),
+          end: new Date(session.endDate),
+          editable: false,
+          deletable: false,
+          draggable: false,
+        })
+      )
+      console.log(sessions)
+      setScheduledSessions(sessions);
+      setEvents(sessions)
+    }).catch((err) => {
+      showErrorToast(err);
+    });
+  }, [])
+
+  return (
+    <>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6 p-3">
+          <h1 className='font-bold text-xl'>Step {step}</h1>
+          {/* this is the title of the auction session. */}
+          {step == 1 && <>
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Auction Session" {...field} />
+                  </FormControl>
+                  <FormDescription>This is the title of the auction session.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea placeholder="description of the session" {...field} />
+                  </FormControl>
+                  <FormDescription>This is the description of the auction session.</FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            {/* This is the attachments for the auction session. */}
+            <ScrollArea className="w-full h-64 overflow-hidden">
+              <FormField
+                control={form.control}
+                name="files"
+                render={({ field }) => <DropzoneComponent {...field} control={form.control} />}
+              />
+            </ScrollArea>
+            <Button type="button" onClick={goNextStep}>Next</Button>
+          </>}
+
+          {step == 2 && <>
+            <div className="flex items-center justify-start gap-4">
+              {/* This is the start date of the auction session. */}
+              <FormField
+                control={form.control}
+                name="startDate"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>Start Date</FormLabel>
+                    <FormControl onChange={handleDateChange}>
+                      <input type="datetime-local" className="cursor-pointer bg-background text-foreground" {...field} />
+                    </FormControl>
+                    {/* {form.control._formState.errors.startDate && <p className='text-red-600'>{form.control._formState.errors.startDate.message}</p>} */}
+                    <FormDescription>this is the start date of the auction session</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* This is the end date of the auction session. */}
+              <FormField
+                control={form.control}
+                name="endDate"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>End Date</FormLabel>
+                    <FormControl onChange={handleDateChange}>
+                      <input type="datetime-local" className="cursor-pointer bg-background text-foreground" {...field} />
+                    </FormControl>
+                    <FormDescription>this is the end date of the auction session</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <Button variant='outline' className='mr-3' type="button" onClick={handleViewSchedule}>Togle Schedule</Button>
+
+            </div>
+
+            {scheduleTrigger &&
+              <Scheduler
+                // onConfirm={async (event, action) => {
+                //   handleOnConfirm(event, action);
+                //   console.log(event)
+                //   console.log(action)
+                //   return await event;
+                // }}
+                // view="week"
+                // day={{
+                //   startHour: 0,
+                //   endHour: 24,
+                //   step: 60
+                // }}
+                week={{
+                  weekDays: [0, 1, 2, 3, 4, 5, 6],
+                  weekStartOn: 1,
+                  startHour: 0,
+                  endHour: 24,
+                  step: 60
+                }}
+                editable={false}
+                events={events}
+              // fields={[
+              //   {
+              //     name: "title",
+              //     type: "input",
+              //     default: form.control._formValues["title"],
+              //     config: { label: "Title", required: true }
+              //   }
+              // ]}
+              />
+            }
+
+            <Button variant='outline' className='mr-3' type="button" onClick={goPreviousStep}>Back</Button>
+
+            {isSubmitting ? (
+              <Button variant="default" disabled>
+                <Loader2 className="animate-spin" size={24} />
+                Submitting...
+              </Button>
+            ) : (
+              <Button type="submit" variant="default">
+                Submit
+              </Button>
+            )}
+          </>}
+        </form>
+
+
+        <ConfirmationDialog
+          open={showTrigger}
+          description="Are you sure to create this auction session?"
+          onOpenChange={setShowTrigger}
+          title="Are you sure to create this auction session?"
+          message={
+            form.formState.isDirty
+              ? `Auction Session ${form.getValues()?.title || ''} will be created.\n Start Date: ${form.getValues()?.startDate ? formatDateTime(form.getValues()?.startDate) : 'N/A'} \n End Date: ${form.getValues()?.endDate ? formatDateTime(form.getValues().endDate) : 'N/A'}`
+              : ''
+          }
+          label="Confirm"
+          onSuccess={confirm}
+        />
+      </Form>
+    </>
   );
 }
