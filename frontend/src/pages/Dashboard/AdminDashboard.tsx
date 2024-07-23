f6e1a02c8d3f100eceac791debf3b26d0250ab47
Duy Anh <143704010+anhhd45@users.noreply.github.com>
2024-07-24 06:31:30 +0700

    Fix admin dashboard

--- frontend/src/pages/Dashboard/AdminDashboard.tsx
+++ frontend/src/pages/Dashboard/AdminDashboard.tsx
@@ -199,12 +199,6 @@ const AdminDashboard = () => {
           </MainCard>
         </Grid>
 
-        {/* row 2 */}
-        <Grid item xs={12} md={7} lg={8}>
-          <UniqueVisitorCard />
-          {/* <LineChart/> */}
-        </Grid>
-
         {/* row 3 */}
 
         {/* <Grid item xs={12} md={5} lg={4}>
@@ -233,92 +227,6 @@ const AdminDashboard = () => {
         </MainCard>
       </Grid> */}
 
-        {/* ROW 4 */}
-        <Grid item xs={12} md={5} lg={4}>
-          <Grid container alignItems="center" justifyContent="space-between">
-            <Grid item>
-              <Typography variant="h6">Transaction History</Typography>
-            </Grid>
-            <Grid item />
-          </Grid>
-          <MainCard sx={{ mt: 2 }} content={false}>
-            <List
-              component="nav"
-              sx={{
-                px: 0,
-                py: 0,
-                '& .MuiListItemButton-root': {
-                  py: 1.5,
-                  '& .MuiAvatar-root': avatarSX,
-                  '& .MuiListItemSecondaryAction-root': {
-                    ...actionSX,
-                    position: 'relative',
-                  },
-                },
-              }}
-            >
-              <ListItemButton divider>
-                <ListItemAvatar>
-                  <Avatar sx={{ color: 'success.main', bgcolor: 'success.lighter' }}>{/* <GiftOutlined /> */}</Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={<Typography variant="subtitle1">Order #002434</Typography>}
-                  secondary="Today, 2:00 AM"
-                />
-                <ListItemSecondaryAction>
-                  <Stack alignItems="flex-end">
-                    <Typography variant="subtitle1" noWrap>
-                      + $1,430
-                    </Typography>
-                    <Typography variant="h6" color="secondary" noWrap>
-                      78%
-                    </Typography>
-                  </Stack>
-                </ListItemSecondaryAction>
-              </ListItemButton>
-              <ListItemButton divider>
-                <ListItemAvatar>
-                  <Avatar sx={{ color: 'primary.main', bgcolor: 'primary.lighter' }}>
-                    {/* <MessageOutlined /> */}
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={<Typography variant="subtitle1">Order #984947</Typography>}
-                  secondary="5 August, 1:45 PM"
-                />
-                <ListItemSecondaryAction>
-                  <Stack alignItems="flex-end">
-                    <Typography variant="subtitle1" noWrap>
-                      + $302
-                    </Typography>
-                    <Typography variant="h6" color="secondary" noWrap>
-                      8%
-                    </Typography>
-                  </Stack>
-                </ListItemSecondaryAction>
-              </ListItemButton>
-              <ListItemButton>
-                <ListItemAvatar>
-                  <Avatar sx={{ color: 'error.main', bgcolor: 'error.lighter' }}>{/* <SettingOutlined /> */}</Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={<Typography variant="subtitle1">Order #988784</Typography>}
-                  secondary="7 hours ago"
-                />
-                <ListItemSecondaryAction>
-                  <Stack alignItems="flex-end">
-                    <Typography variant="subtitle1" noWrap>
-                      + $682
-                    </Typography>
-                    <Typography variant="h6" color="secondary" noWrap>
-                      16%
-                    </Typography>
-                  </Stack>
-                </ListItemSecondaryAction>
-              </ListItemButton>
-            </List>
-          </MainCard>
-        </Grid>
 
         {/* grid bracket */}
       </Grid>
